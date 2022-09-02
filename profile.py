702471be34544d059cf446c323ea6375fcd6d7da
djrivas7400 <85266216+djrivas7400@users.noreply.github.com>
2022-09-02 16:30:18 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -26,7 +26,7 @@ for i in range(num_nodes):
     node = request.XenVM("head")
   else:
     node = request.XenVM("worker-" + str(i))
-  node.cores = 8
+  node.cores = 4
   node.ram = 8192
   node.routable_control_ip = "true" 
   node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
@@ -34,15 +34,14 @@ for i in range(num_nodes):
   iface.component_id = "eth1"
   iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
   link.addInterface(iface)
-  
-  # setup Docker
-  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
-  # setup Kubernetes
+
   node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_kubernetes.sh"))
   node.addService(pg.Execute(shell="sh", command="sudo swapoff -a"))
-  
+  node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/install_docker.sh"))
+
   if i == 0:
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_manager.sh"))
+    node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/launch_jenkins.sh"))
   else:
     node.addService(pg.Execute(shell="sh", command="sudo bash /local/repository/kube_worker.sh"))
     
